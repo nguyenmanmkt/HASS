16cd049fb1f7cd7494e56749769f988fed1c50d9
nguyenmanmkt <129373032+nguyenmanmkt@users.noreply.github.com>
2024-02-27 16:00:11 +0700

    2

--- k8s/master-node1/install-master-1(ubuntu22).sh
+++ k8s/master-node1/install-master-1(ubuntu22).sh
@@ -2,7 +2,6 @@
 
 # Configure hostname and /etc/hosts
 sudo hostnamectl set-hostname master-node1
-
 echo "192.168.200.230 master-node1 master1" | sudo tee -a /etc/hosts
 echo "192.168.200.231 master-node2 master2" | sudo tee -a /etc/hosts
 echo "192.168.200.232 master-node3 master3" | sudo tee -a /etc/hosts
@@ -18,8 +17,11 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 # Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo apt install docker.io -y
+
+sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+
+
 
 # Check Docker status
 sudo systemctl status docker
@@ -31,29 +33,56 @@ EOF
 
 # Add Kubernetes GPG key
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/kubernetes-archive-keyring.gpg add -
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
+sudo apt-get update
 
 # Install Kubernetes components
 sudo apt update
 sudo apt install -y kubelet kubectl kubeadm
-
+# Auto update
+sudo apt-mark hold kubeadm kubelet kubectl
 # Disable swap
-sudo sed -i '/swap/d' /etc/fstab
 sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-# Disable SELinux
-sudo setenforce 0
-sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
-# Configure sysctl settings
-sudo tee /etc/sysctl.d/k8s.conf <<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
 EOF
 
-# module br_netfilter
+sudo modprobe overlay
 sudo modprobe br_netfilter
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Initialize Kubernetes cluster
+sudo kubeadm init --pod-network-cidr=10.10.0.0/16
+
+# Enable kubelet service
+# sudo systemctl enable kubelet
+# sudo systemctl start kubelet
+# sudo systemctl status kubelet
+
+# Create kubeconfig directory cluser
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Deploy Calico network plugin - cluster
+kubectl apply -f https://docs.projectcalico.org/v3.25/manifests/calico.yaml
+
+
+# Check cluster status
+sudo kubectl get nodes
+sudo kubectl get pods --all-namespaces
 
 # Configure firewall cluster
 sudo ufw allow 6443/tcp
@@ -67,26 +96,6 @@ sudo ufw --force enable
 # For Worker Nodes
 # sudo ufw allow 10251/tcp
 # sudo ufw allow 10255/tcp
-
-# Initialize Kubernetes cluster
-sudo kubeadm init --pod-network-cidr=172.24.199.0/24
-
-# Enable kubelet service
-sudo systemctl enable kubelet
-sudo systemctl start kubelet
-
-# Create kubeconfig directory cluser
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# Deploy Calico network plugin - cluster
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
-sudo kubectl apply -f calico.yaml
-
-# Check cluster status
-sudo kubectl get nodes
-sudo kubectl get pods --all-namespaces
 
 # Print join command for worker nodes master
 sudo kubeadm token create --print-join-command
